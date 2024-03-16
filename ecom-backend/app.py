0962b06a663280386b70208fa9dbb234e0c4fe0f
Sunil Yadav <sunil.yadav@creditfair.in>
2024-03-16 13:48:32 +0530

    Starter template

--- ecom-backend/app.py
+++ ecom-backend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 #Flask initialization
 app = Flask(__name__)
 app.secret_key = "ecommerce"
 app.config['SECRET_KEY'] = "ecommerce"
 
+@app.route("/")
+def index():
+    return render_template("home.html")
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5006, debug=True)
+    app.run(host='0.0.0.0', port=5006, debug=False)
     # app.run()
